1832830b6a67227b091770bf6f08a14367324e8d
Leo Taylor <leotaylor723@gmail.com>
2018-03-26 21:13:45 -0500

    prints names

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("testing");
+
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+};
+
+const buildDomString = animalArray => {
+    let domString = "";
+    animalArray.forEach((animal) => {
+        domString += `<h1>${animal.name}</h1>`;
+    })
+    printToDom(domString, 'animal-holder')
+};
+
+function WTF(){
+    console.log("something went wrong");
+}
+
+function executeFunction (){
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.animals);
+}
+
+const startApplication = () =>{
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeFunction);
+    myRequest.addEventListener("error", WTF);
+    myRequest.open("GET", "animals.json");
+    myRequest.send();
+};
+
+startApplication();
